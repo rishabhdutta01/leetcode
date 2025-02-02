7ea67f4777811d8fc1bfad93c57072c8b523aa77
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-02-01 16:29:58 -0800

    Time: 16 ms (6.26%) | Memory: 45.3 MB (6.02%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -13,7 +13,8 @@
  * }
  * }
  */
-class Solution {
+ //Optimal
+ class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(subRoot == null) {
             return true;
@@ -21,22 +22,41 @@ class Solution {
         if(root == null){
             return false;
         }
-        if(isSame(root,subRoot)){
-            return true;
-        }
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        return serialize(root).contains(serialize(subRoot));
     }
 
-    public boolean isSame(TreeNode r, TreeNode sr) {
-        if(r == null && sr == null) {
-            return true;
-        } else if(r == null || sr == null) {
-            return false;
+    public String serialize(TreeNode x) {
+        if(x == null) {
+            return "N";
         }
-        if(r.val == sr.val) {
-            return isSame(r.left, sr.left) && isSame(r.right, sr.right);
-        }else{
-            return false;
-        }
+        return "("+ x.val + "," + serialize(x.left) + "," + serialize(x.right) + ")";
     }
 }
+ //Brute force
+// class Solution {
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         if(subRoot == null) {
+//             return true;
+//         }
+//         if(root == null){
+//             return false;
+//         }
+//         if(isSame(root,subRoot)){
+//             return true;
+//         }
+//         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+//     }
+
+//     public boolean isSame(TreeNode r, TreeNode sr) {
+//         if(r == null && sr == null) {
+//             return true;
+//         } else if(r == null || sr == null) {
+//             return false;
+//         }
+//         if(r.val == sr.val) {
+//             return isSame(r.left, sr.left) && isSame(r.right, sr.right);
+//         }else{
+//             return false;
+//         }
+//     }
+// }
