d48e3b24eb5abca6c12774dc77cdf2856b2ebfa9
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-02-23 15:34:39 -0800

    Time: 1 ms (89.80%) | Memory: 45 MB (53.29%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,32 +15,24 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root == null){
-            return new ArrayList<List<Integer>>();
-        }
+        List<List<Integer>> levelOrder = new ArrayList<>();
+        if(root==null) return levelOrder;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
 
-        List<List<Integer>> outer = new ArrayList<>();
-        TreeNode curr = root;
-        traverse(curr, 0, outer);
-        return outer;
+        while(!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int levelSize = queue.size();
+
+            for(int i=0; i<levelSize; i++){
+                TreeNode temp = queue.poll();
+                list.add(temp.val);
+                if(temp.left!=null) queue.offer(temp.left);
+                if(temp.right!=null) queue.offer(temp.right);
+            }
+            levelOrder.add(list);
+        }
+        return levelOrder;
     }
-
-    public void traverse(TreeNode curr, int i, List<List<Integer>> outer){
-            if(curr == null) {
-                return;
-            }
-            List<Integer> inner;
-            try{
-                inner = outer.get(i);
-            } catch(Exception e) {
-                inner = new ArrayList<Integer>();
-                outer.add(inner); 
-            }
-
-            inner.add(curr.val);
-
-            traverse(curr.left, i+1, outer);
-            traverse(curr.right, i+1, outer);
-            return;
-        }
 }
