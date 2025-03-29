204a509e336a487cc819895b96e2362caf1873c3
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-03-29 12:36:41 -0700

    Time: 1867 ms (5.02%) | Memory: 54.9 MB (38.72%) - LeetSync

---
+++ 403-frog-jump/frog-jump.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n = stones.length;
+        boolean[] dp = new boolean[n];
+        
+        dp[0] = true;
+        if(stones[1] != 1){
+            return false;
+        }
+        dp[1] = true;
+        
+        List<Set<Integer>> jumps = new ArrayList<>(n);
+        jumps.add(new HashSet<>());
+        jumps.add(new HashSet<>());
+        jumps.get(1).add(1);
+
+        for(int i=2;i<n;i++){
+            jumps.add(i, new HashSet<Integer>());
+            for(int j=1;j<i;j++){
+                if(dp[j]){
+                    for(int k: jumps.get(j)){
+                        if(stones[i] >= stones[j]+k-1 && stones[i] <= stones[j]+k+1){
+                            dp[i] = true;
+                            jumps.get(i).add(stones[i] - stones[j]);
+                        }
+                    }
+                }   
+            }
+        }
+
+        return dp[n-1];
+    }
+}
