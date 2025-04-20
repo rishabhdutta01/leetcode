5184d9f9e399f81b74a39e514044248088d5c986
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-04-20 13:08:46 -0700

    Time: 5 ms (51.14%) | Memory: 43.9 MB (21.47%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,55 +1,21 @@
 class Solution {
     public String simplifyPath(String path) {
-        if(path == null){
-            return "/";
-        }
-        String[] arr = path.split("/");
-        if(arr.length == 0){
-            return "/";
-        }
-        
-        Stack<String> s = new Stack<>();
-        for(int i = arr.length-1; i>=0; i--){
-            if(".".equals(arr[i])){
-                continue;
-            } else if ("..".equals(arr[i])){
-                s.push(arr[i]);
-            } else if("".equals(arr[i])) {
-                 continue;
-            } else {
-                if(s.isEmpty()){
-                    s.push(arr[i]);
-                } else {
-                    if("..".equals(s.peek())){
-                        s.pop();
-                        continue;
-                    } else {
-                        s.push(arr[i]);
-                    }
-                }
-            }
-        }
-        if(s.isEmpty()){
-            return "/";
-        }
+       String[] components=path.split("/");
+       Stack<String> st=new Stack<>();
 
-        StringBuilder sb = new StringBuilder("/");
-        
-        int n = s.size();
-        for(int i = 0; i<n-1;i++){
-            if("..".equals(s.peek())){
-                s.pop();
-                continue;
-            }
-            sb.append(s.pop());
-            sb.append("/");
+       for(String comp:components){
+        if(comp.equals("") || comp.equals(".")) continue;
+        if(comp.equals("..")){
+            if(!st.isEmpty()) st.pop();
         }
+        else st.push(comp);
+       }
 
-        if("..".equals(s.peek())){
-            s.pop();
-        } else {
-            sb.append(s.pop());
-        }
-        return sb.toString();
+       StringBuilder sb=new StringBuilder();
+       while(!st.isEmpty()) sb.insert(0,"/"+st.pop());
+       //while(!st.isEmpty()) sb.insert(0,'/'+st.pop());
+
+       if(sb.length()==0) return "/";
+       return sb.toString(); 
     }
 }
