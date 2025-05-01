ff01ac6c623ac219149d3606dd243862b97df74c
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-01 10:15:57 -0700

    Time: 7 ms (49.17%) | Memory: 44.7 MB (98.34%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,72 +1,35 @@
 class Solution {
-    class Node {
-        int value;
-        boolean visited;
-        List<Node> children;
-        public Node(int value) {
-            this.value = value;
-            this.children = new ArrayList<>();
-        }
-    }
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(numCourses == 0 || numCourses == 1) {
-            return true;
-        }
-        if(prerequisites == null || prerequisites.length ==0) {
-            return true;
-        }
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
 
-        Node[] n = new Node[numCourses];
-        int[] in = new int[numCourses];
-
-        for(int i = 0; i<prerequisites.length; i++) {
-            Node subject = null;
-            if(n[prerequisites[i][0]] == null) {
-                subject = new Node(prerequisites[i][0]);
-                n[prerequisites[i][0]] = subject;
-            } else {
-                subject = n[prerequisites[i][0]];
-            }
-
-            Node prereq = null;
-            if(n[prerequisites[i][1]] == null) {
-                prereq = new Node(prerequisites[i][1]);
-                n[prerequisites[i][1]] = prereq;
-            } else {
-                prereq = n[prerequisites[i][1]];
-            }
-            
-            in[prerequisites[i][1]]++;
-            subject.children.add(prereq);
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> toposort = new ArrayList<>();
-
-        for(int i=0;i<in.length;i++){
-            if(in[i] == 0){
-                q.offer(i);
-            }
-        }
-        while(!q.isEmpty()){
-           int x= q.poll();
-            Node parent = n[x];
-            if(parent == null){
-                toposort.add(x);
-                continue;
-            }
-            toposort.add(parent.value);
-            for(Node curr: parent.children) {
-                in[curr.value]--;
-                if(in[curr.value]==0){
-                    q.offer(curr.value);
-                }
-            }
+        for (int[] prerequisite : prerequisites) {
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+            indegree[prerequisite[0]]++;
         }
 
-        if(toposort.size()!=n.length)
-            return false;
-        return true;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) queue.add(i);
+        }
+
+        int nodesVisited = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            nodesVisited++;
+
+            for (int neighbor : adj.get(curr)) {
+                indegree[neighbor] --;
+                if (indegree[neighbor] == 0) 
+                    queue.add(neighbor);
+            }
+
+        }
+
+        return nodesVisited == numCourses;
     }
 }
