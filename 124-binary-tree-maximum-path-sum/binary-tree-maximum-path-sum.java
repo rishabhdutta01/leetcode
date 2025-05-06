9f10c1977203951e14e1b5d81531f42182256187
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-06 15:33:22 -0700

    Time: 0 ms (100.00%) | Memory: 44.5 MB (36.48%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -14,73 +14,45 @@
  * }
  */
 
- class Solution {
-    int max = Integer.MIN_VALUE;
+class Solution {
+    int res = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        fnc(root);
-        // max= Math.max(max,fnc(root));
-        return max;
+        if(root==null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return root.val;
+        }
+
+        TreeNode curr = root;
+        find(curr);
+        return res;
     }
 
-    int fnc(TreeNode curr){
+    int find(TreeNode curr){
         if(curr == null){
             return 0;
         }
-        int lsum = fnc(curr.left);
-        int rsum = fnc(curr.right);
-        max = Math.max(max, 
-                    Math.max(lsum+curr.val, 
-                    Math.max(rsum+curr.val, 
-                    Math.max(lsum+rsum+curr.val, curr.val))));
-        return Math.max(lsum+curr.val, 
-                    Math.max(rsum+curr.val, curr.val));
-    }
- }
+        if(curr.left == null && curr.right == null){
+            res = Math.max(res, curr.val);
+            if(curr.val<0){
+                return 0;
+            }
+            return curr.val;
+        }
 
-
-
-
-
-
-// class Solution {
-//     int res = Integer.MIN_VALUE;
-//     public int maxPathSum(TreeNode root) {
-//         if(root==null){
-//             return 0;
-//         }
-//         if(root.left == null && root.right == null){
-//             return root.val;
-//         }
-
-//         TreeNode curr = root;
-//         find(curr);
-//         return res;
-//     }
-
-//     int find(TreeNode curr){
-//         if(curr == null){
-//             return 0;
-//         }
-//         if(curr.left == null && curr.right == null){
-//             res = Math.max(res, curr.val);
-//             if(curr.val<0){
-//                 return 0;
-//             }
-//             return curr.val;
-//         }
-
-//         int l = find(curr.left);
-//         int r = find(curr.right);
+        int l = find(curr.left);
+        int r = find(curr.right);
         
-//         res = Math.max(Math.max(Math.max(res, r+curr.val),l+curr.val), l + curr.val + r);
+        res = Math.max(Math.max(Math.max(res, r+curr.val),l+curr.val), l + curr.val + r);
 
-//         if(l>=r){
-//             if(l+curr.val > 0)
-//                 return l+curr.val;
-//             return 0;
-//         }
-//         if(r+curr.val > 0)
-//             return r+curr.val;
-//         return 0;
-//     }
-// }
+        if(l>=r){
+            if(l+curr.val > 0)
+                return l+curr.val;
+            return 0;
+        }
+        if(r+curr.val > 0)
+            return r+curr.val;
+        return 0;
+    }
+}
