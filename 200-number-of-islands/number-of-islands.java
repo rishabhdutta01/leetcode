df9124a9097643c1a3903db24c2e7acc014009c3
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-05 22:45:05 -0700

    Time: 4 ms (48.55%) | Memory: 52.4 MB (16.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -4,14 +4,12 @@ class Solution {
             return 0;
         }
 
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-
         int res = 0;
 
         for(int i=0;i<grid.length;i++) {
             for(int j=0;j<grid[0].length;j++) {
-                if(grid[i][j] == '1' && visited[i][j] == false) {
-                    go(grid, visited, i, j);
+                if(grid[i][j] == '1') {
+                    go(grid, i, j);
                     res+=1;
                 }
             }
@@ -19,15 +17,53 @@ class Solution {
         return res;
     }
 
-    void go(char[][] grid, boolean[][] visited, int i, int j) {
-        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length || grid[i][j] == '0' || visited[i][j] == true){
+    void go(char[][] grid, int i, int j) {
+        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length || grid[i][j] == '0'){
             return;
         }
+
+        grid[i][j] = '0';
+
+        int[][] moves = {{0,-1},{0,1},{-1,0},{1,0}};
         
-        visited[i][j] = true;
-        go(grid, visited, i,j-1);
-        go(grid, visited, i,j+1);
-        go(grid, visited, i+1,j);
-        go(grid, visited, i-1,j);
+        for(int k=0;k<moves.length;k++){
+            go(grid,i+moves[k][0],j+moves[k][1]);
+        }        
+        return;
     }
 }
+
+//DFS
+// class Solution {
+//     public int numIslands(char[][] grid) {
+//         if(grid == null || grid.length == 0) {
+//             return 0;
+//         }
+
+//         boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+//         int res = 0;
+
+//         for(int i=0;i<grid.length;i++) {
+//             for(int j=0;j<grid[0].length;j++) {
+//                 if(grid[i][j] == '1' && visited[i][j] == false) {
+//                     go(grid, visited, i, j);
+//                     res+=1;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+
+//     void go(char[][] grid, boolean[][] visited, int i, int j) {
+//         if(i<0 || i>= grid.length || j<0 || j>= grid[0].length || grid[i][j] == '0' || visited[i][j] == true){
+//             return;
+//         }
+        
+//         visited[i][j] = true;
+//         go(grid, visited, i,j-1);
+//         go(grid, visited, i,j+1);
+//         go(grid, visited, i+1,j);
+//         go(grid, visited, i-1,j);
+//     }
+// }
