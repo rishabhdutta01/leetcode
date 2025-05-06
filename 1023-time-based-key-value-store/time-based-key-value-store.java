f6b79e1a6051006e6552f5f5e8f5335d9fa9b80a
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-06 13:41:50 -0700

    Time: 157 ms (27.88%) | Memory: 121.9 MB (36.03%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,32 +1,79 @@
 class TimeMap {
-
-    Map<String,TreeMap<Integer,String>> m;
+    HashMap<String, ArrayList<Pair<Integer, String>>> keyTimeMap;
+    
     public TimeMap() {
-        m = new HashMap<>();
+        keyTimeMap = new HashMap();
     }
     
     public void set(String key, String value, int timestamp) {
-        TreeMap<Integer, String> tm;
-        if(!m.containsKey(key)){
-            tm = new TreeMap<>();
-            m.put(key, tm);
-        } else{
-            tm=m.get(key);
+        if (!keyTimeMap.containsKey(key)) {
+            keyTimeMap.put(key, new ArrayList());
         }
-        tm.put(timestamp,value);
-
+        
+        // Store '(timestamp, value)' pair in 'key' bucket.
+        keyTimeMap.get(key).add(new Pair(timestamp, value));
     }
     
     public String get(String key, int timestamp) {
-        if(!m.containsKey(key)){
+        // If the 'key' does not exist in map we will return empty string.
+        if (!keyTimeMap.containsKey(key)) {
             return "";
         }
-        TreeMap<Integer, String> tm = m.get(key);
-        Integer floorKey = tm.floorKey(timestamp);
-        if(floorKey == null) return "";
-        return tm.get(floorKey);
+        
+        if (timestamp < keyTimeMap.get(key).get(0).getKey()) {
+            return "";
+        }
+        
+        // Using binary search on the list of pairs.
+        int left = 0;
+        int right = keyTimeMap.get(key).size();
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (keyTimeMap.get(key).get(mid).getKey() <= timestamp) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        // If iterator points to first element it means, no time <= timestamp exists.
+        if (right == 0) {
+            return "";
+        }
+                
+        return keyTimeMap.get(key).get(right - 1).getValue();
     }
 }
+// class TimeMap {
+
+//     Map<String,TreeMap<Integer,String>> m;
+//     public TimeMap() {
+//         m = new HashMap<>();
+//     }
+    
+//     public void set(String key, String value, int timestamp) {
+//         TreeMap<Integer, String> tm;
+//         if(!m.containsKey(key)){
+//             tm = new TreeMap<>();
+//             m.put(key, tm);
+//         } else{
+//             tm=m.get(key);
+//         }
+//         tm.put(timestamp,value);
+
+//     }
+    
+//     public String get(String key, int timestamp) {
+//         if(!m.containsKey(key)){
+//             return "";
+//         }
+//         TreeMap<Integer, String> tm = m.get(key);
+//         Integer floorKey = tm.floorKey(timestamp);
+//         if(floorKey == null) return "";
+//         return tm.get(floorKey);
+//     }
+// }
 
 /**
  * Your TimeMap object will be instantiated and called as such:
