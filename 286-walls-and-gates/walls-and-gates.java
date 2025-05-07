3ccdd3396905aa3f419438fa71ea7375565b6a24
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-06 20:38:22 -0700

    Time: 15 ms (17.63%) | Memory: 48.7 MB (17.30%) - LeetSync

---
+++ 286-walls-and-gates/walls-and-gates.java
@@ -0,0 +1,48 @@
+class Solution {
+    int inf = 2147483647;
+    public void wallsAndGates(int[][] rooms) {
+        
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+        Set<Pair<Integer,Integer>> s = new HashSet<>();
+        boolean[][] visited = new boolean[rooms.length][rooms[0].length];
+
+        for(int i=0;i<rooms.length;i++){
+            for(int j=0;j<rooms[0].length;j++){
+                if(rooms[i][j] == 0){
+                    q.offer(new Pair(i,j));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        if(q.isEmpty()){
+            return;
+        }
+
+        int[][] moves = {{-1,0},{0,1},{1,0},{0,-1}};
+        
+
+        while(!q.isEmpty()){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                Pair<Integer,Integer> p = q.poll();
+                int row = p.getKey();
+                int col = p.getValue();
+                for(int j=0;j<moves.length;j++){
+                    int nrow = row+moves[j][0];
+                    int ncol = col+moves[j][1];
+                    if(nrow>=0 && nrow<rooms.length && ncol>=0 && ncol<rooms[0].length && rooms[nrow][ncol]!=-1 && !visited[nrow][ncol]){
+                        rooms[nrow][ncol] = rooms[row][col]+1;
+                        s.add(new Pair(nrow,ncol));
+                        visited[nrow][ncol] = true;
+                    }
+                }   
+            }
+
+            for(Pair<Integer, Integer> p : s) {
+                q.offer(p);
+            }
+            s.clear();
+        }
+    }
+}
