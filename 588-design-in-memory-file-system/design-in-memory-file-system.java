05b7371eea0c31a4201f32091d6e697d2bdc5051
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-07 20:51:27 -0700

    Time: 40 ms (65.09%) | Memory: 45.8 MB (74.16%) - LeetSync

--- 588-design-in-memory-file-system/design-in-memory-file-system.java
+++ 588-design-in-memory-file-system/design-in-memory-file-system.java
@@ -1,59 +1,61 @@
 public class FileSystem {
-    class Dir {
-        HashMap < String, Dir > dirs = new HashMap < > ();
-        HashMap < String, String > files = new HashMap < > ();
+    class File {
+        boolean isfile = false;
+        Map < String, File > files = new TreeMap<> ();
+        String content = "";
     }
-    Dir root;
+    File root;
     public FileSystem() {
-        root = new Dir();
+        root = new File();
     }
+
     public List < String > ls(String path) {
-        Dir t = root;
+        File t = root;
         List < String > files = new ArrayList < > ();
         if (!path.equals("/")) {
             String[] d = path.split("/");
-            for (int i = 1; i < d.length - 1; i++) {
-                t = t.dirs.get(d[i]);
+            for (int i = 1; i < d.length; i++) {
+                t = t.files.get(d[i]);
             }
-            if (t.files.containsKey(d[d.length - 1])) {
+            if (t.isfile) {
                 files.add(d[d.length - 1]);
                 return files;
-            } else {
-                t = t.dirs.get(d[d.length - 1]);
             }
         }
-        files.addAll(new ArrayList < > (t.dirs.keySet()));
-        files.addAll(new ArrayList < > (t.files.keySet()));
-        Collections.sort(files);
-        return files;
+        List < String > res_files = new ArrayList < > (t.files.keySet());
+        return res_files;
     }
 
     public void mkdir(String path) {
-        Dir t = root;
+        File t = root;
         String[] d = path.split("/");
         for (int i = 1; i < d.length; i++) {
-            if (!t.dirs.containsKey(d[i]))
-                t.dirs.put(d[i], new Dir());
-            t = t.dirs.get(d[i]);
+            if (!t.files.containsKey(d[i]))
+                t.files.put(d[i], new File());
+            t = t.files.get(d[i]);
         }
     }
 
     public void addContentToFile(String filePath, String content) {
-        Dir t = root;
+        File t = root;
         String[] d = filePath.split("/");
         for (int i = 1; i < d.length - 1; i++) {
-            t = t.dirs.get(d[i]);
+            t = t.files.get(d[i]);
         }
-        t.files.put(d[d.length - 1], t.files.getOrDefault(d[d.length - 1], "") + content);
+        if (!t.files.containsKey(d[d.length - 1]))
+            t.files.put(d[d.length - 1], new File());
+        t = t.files.get(d[d.length - 1]);
+        t.isfile = true;
+        t.content = t.content + content;
     }
 
     public String readContentFromFile(String filePath) {
-        Dir t = root;
+        File t = root;
         String[] d = filePath.split("/");
         for (int i = 1; i < d.length - 1; i++) {
-            t = t.dirs.get(d[i]);
+            t = t.files.get(d[i]);
         }
-        return t.files.get(d[d.length - 1]);
+        return t.files.get(d[d.length - 1]).content;
     }
 }
 
