82f2eba92e51810ffd2516f0b169c4497effeba9
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-09 12:14:35 -0700

    Time: 28 ms (46.88%) | Memory: 47.5 MB (63.75%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -60,26 +60,53 @@
 //     }
 // }
 
+// class Solution {
+//     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+//         Arrays.sort(products);
+
+//         List<List<String>> res = new ArrayList<>();
+//         List<String> p = Arrays.asList(products);
+//         for(int i=0;i<searchWord.length();i++){
+//             List<String> newp = new ArrayList<>();
+//             for(int j=0;j<p.size();j++){
+//                 if(i>=p.get(j).length()){
+//                     continue;
+//                 }
+//                 if(searchWord.charAt(i) == p.get(j).charAt(i)){
+//                     newp.add(p.get(j));
+//                 }
+//             }
+//             p = newp;
+
+//             res.add(p.subList(0, Math.min(3, p.size())));
+//         }
+//         return res;
+//     }
+// }
+
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        Arrays.sort(products);
-
         List<List<String>> res = new ArrayList<>();
-        List<String> p = Arrays.asList(products);
-        for(int i=0;i<searchWord.length();i++){
-            List<String> newp = new ArrayList<>();
-            for(int j=0;j<p.size();j++){
-                if(i>=p.get(j).length()){
-                    continue;
-                }
-                if(searchWord.charAt(i) == p.get(j).charAt(i)){
-                    newp.add(p.get(j));
-                }
-            }
-            p = newp;
+        TreeMap<String, Integer> map = new TreeMap<>();
+        Arrays.sort(products);
+        List<String> productsList = Arrays.asList(products);
 
-            res.add(p.subList(0, Math.min(3, p.size())));
+        for (int i = 0; i < products.length; i++) {
+            map.put(products[i], i);
         }
+
+        String key = "";
+        for (char c : searchWord.toCharArray()) {
+            key += c;
+            String ceiling = map.ceilingKey(key);
+            String floor = map.floorKey(key + "~");
+            if (ceiling == null || floor == null)
+                break;
+            res.add(productsList.subList(map.get(ceiling), Math.min(map.get(ceiling) + 3, map.get(floor) + 1)));
+        }
+
+        while (res.size() < searchWord.length())
+            res.add(new ArrayList<>());
         return res;
     }
 }
