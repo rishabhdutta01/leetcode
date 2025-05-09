ed2ca9f4a4e1137005d22ed5cf5fa7638a3aa7bb
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-08 18:22:59 -0700

    Time: 8 ms (98.98%) | Memory: 56 MB (98.28%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,43 +1,60 @@
+//DP
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int res = Integer.MIN_VALUE;
-        Stack<Integer> s= new Stack<>();
-        s.push(-1);
-        for(int i=0;i<n;i++) {
-            while(s.peek()!=-1 && heights[i]<heights[s.peek()]){
-                int h = heights[s.pop()];
-                int w = i-s.peek()-1;
-                res = Math.max(res,h*w);
+    public int largestRectangleArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int[] lessFromLeft = new int[height.length]; // idx of the first bar on left that is lower than current
+        int[] lessFromRight = new int[height.length]; // idx of the first bar on right that is lower than current
+        lessFromRight[height.length - 1] = height.length;
+        lessFromLeft[0] = -1;
+
+        for (int i = 1; i < height.length; i++) {
+            int p = i - 1;
+            while (p >= 0 && height[p] >= height[i]) {
+                p = lessFromLeft[p];
             }
-            s.push(i);
+            lessFromLeft[i] = p;
         }
 
-        while(s.peek() != -1){
-            int h = heights[s.pop()];
-            int w = n-s.peek()-1;
-            res = Math.max(res,h*w);
+        for (int i = height.length - 2; i >= 0; i--) {
+            int p = i + 1;
+            while (p < height.length && height[p] >= height[i]) {
+                p = lessFromRight[p];
+            }
+            lessFromRight[i] = p;
         }
-        return res;
+
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            maxArea = Math.max(maxArea, height[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        }
+
+        return maxArea;
     }
 }
 
+//Monotonic stack
 // class Solution {
 //     public int largestRectangleArea(int[] heights) {
 //         int n = heights.length;
-//         int[][] min = new int[n][n];
-//         for(int j=0;j<n;j++){
-//             min[j][j] = heights[j];
-//             for(int i=j-1;i>=0;i--){
-//                 min[i][j] = Math.min(min[i+1][j], min[i][j-1]);
+//         int res = Integer.MIN_VALUE;
+//         Stack<Integer> s= new Stack<>();
+//         s.push(-1);
+//         for(int i=0;i<n;i++) {
+//             while(s.peek()!=-1 && heights[i]<heights[s.peek()]){
+//                 int h = heights[s.pop()];
+//                 int w = i-s.peek()-1;
+//                 res = Math.max(res,h*w);
 //             }
+//             s.push(i);
 //         }
-//         int res= Integer.MIN_VALUE;
-//         for(int j=0;j<n;j++){
-//             for(int i=j-1;i>=0;i--){
-//                 min[i][j] = Math.max(min[i][j]*(j-i+1),Math.max(min[i+1][j], min[i][j-1]));
-//             }
+
+//         while(s.peek() != -1){
+//             int h = heights[s.pop()];
+//             int w = n-s.peek()-1;
+//             res = Math.max(res,h*w);
 //         }
-//         return min[0][n-1];
+//         return res;
 //     }
 // }
