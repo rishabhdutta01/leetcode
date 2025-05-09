bb25f6c62b2efacf37687ed6893d9bc4583cd71a
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-09 14:37:23 -0700

    Time: 56 ms (31.00%) | Memory: 64.2 MB (15.58%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -3,6 +3,7 @@ class Solution {
         int st;
         int et;
         int profit;
+
         Job(int st, int et, int profit) {
             this.st = st;
             this.et = et;
@@ -15,25 +16,25 @@ class Solution {
         for (int i = 0; i < startTime.length; i++) {
             jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
         }
-        Arrays.sort(jobs, (a,b) -> a.st - b.st);
+        Arrays.sort(jobs, (a, b) -> a.st - b.st);
 
         Map<String, Integer> memo = new HashMap<>();
         return fnc(jobs, 0, memo);
     }
 
     int fnc(Job[] jobs, int idx, Map<String, Integer> memo) {
-        if(idx == jobs.length) {
+        if (idx == jobs.length) {
             return 0;
         }
 
         String key = String.valueOf(idx);
-        if(memo.containsKey(key)) {
+        if (memo.containsKey(key)) {
             return memo.get(key);
         }
 
         int nextIdx = findNextIdx(jobs, idx);
         int pick = jobs[idx].profit + fnc(jobs, nextIdx, memo);
-        int notpick = fnc(jobs, idx+1, memo);
+        int notpick = fnc(jobs, idx + 1, memo);
 
         int result = Math.max(pick, notpick);
         memo.put(key, result);
@@ -42,13 +43,12 @@ class Solution {
 
     int findNextIdx(Job[] jobs, int idx) {
         int target = jobs[idx].et;
-        int left = idx + 1;  // Start from next index
-        int right = jobs.length;  // Use length as right boundary
-        
-        // Find first job that starts at or after current job's end time
-        while(left < right) {
+        int left = idx + 1;
+        int right = jobs.length;
+
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if(jobs[mid].st < target) {
+            if (jobs[mid].st < target) {
                 left = mid + 1;
             } else {
                 right = mid;
