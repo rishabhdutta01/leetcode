d39534fa4de775e840b1a3fb8a22d54c76956b49
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-05-10 14:45:18 -0700

    Time: 0 ms (100.00%) | Memory: 42.8 MB (22.11%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -3,7 +3,9 @@ public class Solution {
         int l = 0, r = nums.length - 1;
         while (l < r) {
             int mid = (l + r) / 2;
+            //decreasing seq thus search on left
             if (nums[mid] > nums[mid + 1]) r = mid;
+            //increasing seq thus search on right
             else l = mid + 1;
         }
         return l;
