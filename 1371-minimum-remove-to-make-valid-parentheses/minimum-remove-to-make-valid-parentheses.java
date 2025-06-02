0d695fa51ba49a53cc56a4ac71d81a00c2187259
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-02 16:01:52 -0700

    Time: 7 ms (97.97%) | Memory: 45.6 MB (44.13%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,40 +1,66 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        if(s==null || s.length()==0) return "";
-
-        int open=0;
-        int close=0;
-        int opentotal=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '('){
+        char[] arr = s.toCharArray();
+        int open=0, opentotal=0, i=0, j=0;
+        for(i=0;i<arr.length;i++){
+            if(arr[i]=='('){
                 open++;
                 opentotal++;
-            } else if(s.charAt(i) == ')'){
-                if(open==0) close++;
-                else open--;
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        int openseen=0;
-        int closedseen=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '('){
-                if(opentotal != open) {
-                    openseen++;
-                    sb.append(s.charAt(i));
-                    opentotal--;
+            } else if(arr[i]==')'){
+                if(open>0){
+                    open--;       
+                } else{
+                    continue;   
                 }
-            } else if(s.charAt(i) == ')'){
-                if(openseen==0) close--;
-                else {
-                    openseen--;
-                    sb.append(s.charAt(i));
-                }
-            }else{
-                sb.append(s.charAt(i));
             }
+            arr[j] = arr[i];
+            j++;
         }
-        return sb.toString();
+        int openkeep = opentotal - open;
+        int n = j;
+        j=0;  
+        for(i=0;i<n;i++){
+            if(arr[i]=='('){
+                if(openkeep > 0){
+                    openkeep--;
+                } else continue;
+            } 
+            arr[j] = arr[i];
+            j++;
+        }
+        return new String(arr, 0, j);
     }
 }
+
+// Time - O(n), space - O(n)
+// class Solution {
+//     public String minRemoveToMakeValid(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         int r=0;
+//         int l=0;
+
+//         //Remove faulty close brackets
+//         for(int i=0;i<s.length();i++){
+//             char c = s.charAt(i);
+//             if(c=='('){
+//                 l++;
+//             } else if(c==')'){
+//                 if(l>0) l--;
+//                 else continue;
+//             }
+//             sb.append(c);
+//         }
+
+//         //Remove faulty open brackets
+//         int i = sb.length()-1;
+//         while(i>=0){
+//             if(sb.charAt(i)=='(' && l>0){
+//                 l--;
+//                 sb.deleteCharAt(i);
+//             } 
+//             i--;
+//         }
+
+//         return sb.toString();
+//     }
+// }
