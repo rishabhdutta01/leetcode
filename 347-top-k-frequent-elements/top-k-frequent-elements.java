383eeb2985aa3016244c5bf6e31cbec77d5c6166
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-07 14:54:19 -0700

    Time: 16 ms (21.48%) | Memory: 48 MB (80.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,95 +1,63 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i =0;i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
-        }
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-
-        for(int key: map.keySet()){
-            if(pq.size() == k){
-                if(pq.peek()[1] < map.get(key)) {
-                    pq.poll();
-                    pq.add(new int[] {key, map.get(key)});
-                }
-            } else{
-                pq.add(new int[] {key, map.get(key)});
+        if(nums.length == 1) {
+            if(k==1){
+                return nums;
             }
-            
-            
+            return null;
         }
 
-        int[] res = new int[k];
-        int i=0;
-        while(!pq.isEmpty()){
-            res[i] = pq.poll()[0];
-            i++;
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = -1;
+
+        for(int i=0; i<nums.length; i++){
+            if(map.get(nums[i]) == null) {
+                map.put(nums[i], 0);
+            }
+            int n = map.get(nums[i]) + 1;
+            max = Math.max(max, n);
+            map.put(nums[i], n);
         }
-        return res;
+
+        if(map.size() == k) {
+            int[] arr = new int[k];
+            int i=0;
+            for(Integer key: map.keySet()){           
+                arr[i] = key;
+                i++;
+            }
+            return arr;
+        }
+
+        List<List<Integer>> list = new ArrayList<>(max+1);
+        for (int i = 0; i <= max; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for(Integer key: map.keySet()){
+            int val = map.get(key);
+            List<Integer> l = list.get(val);
+            l.add(key);
+            list.set(val, l);
+        }
+
+        int[] arr = new int[k];
+        int i = 0;
+
+        while(i<k) {
+            List<Integer> l = list.get(max);
+            if(l != null){
+                for (int j = 0; j < l.size(); j++) {
+                    if(i == k) {
+                        break;
+                    }
+                    arr[i]=l.get(j);
+                    i++;
+                }
+            }
+            max--;
+        }
+
+        return arr;
     }
 }
-
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         if(nums.length == 1) {
-//             if(k==1){
-//                 return nums;
-//             }
-//             return null;
-//         }
-
-//         Map<Integer, Integer> map = new HashMap<>();
-//         int max = -1;
-
-//         for(int i=0; i<nums.length; i++){
-//             if(map.get(nums[i]) == null) {
-//                 map.put(nums[i], 0);
-//             }
-//             int n = map.get(nums[i]) + 1;
-//             max = Math.max(max, n);
-//             map.put(nums[i], n);
-//         }
-
-//         if(map.size() == k) {
-//             int[] arr = new int[k];
-//             int i=0;
-//             for(Integer key: map.keySet()){           
-//                 arr[i] = key;
-//                 i++;
-//             }
-//             return arr;
-//         }
-
-//         List<List<Integer>> list = new ArrayList<>(max+1);
-//         for (int i = 0; i <= max; i++) {
-//             list.add(new ArrayList<>());
-//         }
-
-//         for(Integer key: map.keySet()){
-//             int val = map.get(key);
-//             List<Integer> l = list.get(val);
-//             l.add(key);
-//             list.set(val, l);
-//         }
-
-//         int[] arr = new int[k];
-//         int i = 0;
-
-//         while(i<k) {
-//             List<Integer> l = list.get(max);
-//             if(l != null){
-//                 for (int j = 0; j < l.size(); j++) {
-//                     if(i == k) {
-//                         break;
-//                     }
-//                     arr[i]=l.get(j);
-//                     i++;
-//                 }
-//             }
-//             max--;
-//         }
-
-//         return arr;
-//     }
-// }
