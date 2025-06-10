62b05ff976263c1d511c1287df8cd404b7563be6
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-09 19:03:25 -0700

    Time: 25 ms (10.60%) | Memory: 55.4 MB (5.41%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,10 +1,10 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        Map<String,Integer> map = new HashMap<>();
-        return fnc(s,t,0,0,map);
+        Integer[][] dp = new Integer[s.length()][t.length()];
+        return fnc(s,t,0,0,dp);
     }
 
-    int fnc(String s, String t, int si, int ti, Map<String,Integer> map){
+    int fnc(String s, String t, int si, int ti, Integer[][] dp){
         if(si == s.length()){
             if(ti == t.length()) return 1;
             else return 0;
@@ -12,19 +12,17 @@ class Solution {
 
         if(ti == t.length()) return 1;
 
-        String key = si + "," + ti;
-        if(map.containsKey(key)) return map.get(key);
+        
+        if(dp[si][ti] != null) return dp[si][ti];
 
         int res = 0;
-        for(int i=si;i<s.length();i++){
-            if(s.length() - i < t.length() - ti) break;
-            if(s.charAt(i) != t.charAt(ti)){
-                continue;
-            }else{
-                res += fnc(s,t,i+1,ti+1,map);
-            }
+        if(s.charAt(si) == t.charAt(ti)){
+            res += fnc(s,t,si+1,ti+1,dp);
         }
-        map.put(key, res);
+        
+        res+= fnc(s,t,si+1,ti,dp);
+        
+        dp[si][ti] = res;
         return res;
     }
 }
