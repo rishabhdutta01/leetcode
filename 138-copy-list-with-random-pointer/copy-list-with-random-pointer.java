f024b50300aac0a259bb73f410adbe039e16965c
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-13 21:12:54 -0700

    Time: 0 ms (100.00%) | Memory: 44.7 MB (42.87%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -13,45 +13,85 @@ class Node {
 }
 */
 
+
 class Solution {
     public Node copyRandomList(Node head) {
+        if(head == null) return null;
         Node curr = head;
-        Node prev = null;
-
-        Map<Node, Node> map = new HashMap<>();
-        Map<Node, Node> visited = new HashMap<>();
         while (curr != null) {
-            if (visited.containsKey(curr)) {
-                continue;
-            }
-
-            Node n = null;
-            if (map.containsKey(curr))
-                n = map.get(curr);
-            else {
-                n = new Node(curr.val);
-                map.put(curr, n);
-            }
-            visited.put(curr, n);
-            if (prev != null) {
-                map.get(prev).next = n;
-            }
-
-            if (curr.random != null) {
-                Node r = null;
-                if (map.containsKey(curr.random))
-                    r = map.get(curr.random);
-                else {
-                    r = new Node(curr.random.val);
-                    map.put(curr.random, r);
-                }
-                n.random = r;
-            }
-
-            prev = curr;
-            curr = curr.next;
+            Node n = new Node(curr.val);
+            n.next = curr.next;
+            curr.next = n;
+            curr = n.next;
         }
 
-        return map.get(head);
+        curr = head;
+        while (curr != null) {
+            if(curr.random != null){
+                curr.next.random = curr.random.next;
+            }  
+            curr = curr.next.next;
+        }
+
+                curr = head;
+        Node newHead = curr.next;
+        Node newCurr = newHead;
+        while (curr != null) {
+            curr.next = newCurr.next; // Restore original list's next pointer
+            
+            // Set the next pointer for the copied node, handle the end of the list
+            if (newCurr.next != null) { 
+                newCurr.next = newCurr.next.next;
+            } else {
+                newCurr.next = null; // Set the last copied node's next to null
+            }
+            
+            curr = curr.next; // Move to the next original node
+            newCurr = newCurr.next; // Move to the next copied node
+        }
+        return newHead;
     }
 }
+
+// class Solution {
+//     public Node copyRandomList(Node head) {
+//         Node curr = head;
+//         Node prev = null;
+
+//         Map<Node, Node> map = new HashMap<>();
+//         Map<Node, Node> visited = new HashMap<>();
+//         while (curr != null) {
+//             if (visited.containsKey(curr)) {
+//                 continue;
+//             }
+
+//             Node n = null;
+//             if (map.containsKey(curr))
+//                 n = map.get(curr);
+//             else {
+//                 n = new Node(curr.val);
+//                 map.put(curr, n);
+//             }
+//             visited.put(curr, n);
+//             if (prev != null) {
+//                 map.get(prev).next = n;
+//             }
+
+//             if (curr.random != null) {
+//                 Node r = null;
+//                 if (map.containsKey(curr.random))
+//                     r = map.get(curr.random);
+//                 else {
+//                     r = new Node(curr.random.val);
+//                     map.put(curr.random, r);
+//                 }
+//                 n.random = r;
+//             }
+
+//             prev = curr;
+//             curr = curr.next;
+//         }
+
+//         return map.get(head);
+//     }
+// }
