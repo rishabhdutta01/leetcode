3ea723dcb3b1e1846649d389008c0137c15c20e3
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-15 13:18:57 -0700

    Time: 1 ms (99.92%) | Memory: 44.5 MB (64.24%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,37 +10,37 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-        return mergeLists(lists, 0, lists.length - 1);
+        if(lists==null || lists.length == 0) {
+            return null;
+        }
+        return mergeLists(lists, 0, lists.length-1);
     }
 
-    ListNode mergeLists(ListNode[] lists, int start, int end) {
-        if (start == end) return lists[start];
+    ListNode mergeLists(ListNode[] lists, int start, int end){
+        if(start == end) return lists[start];
         if (start > end) return null;
-        
-        int mid = start + (end - start) / 2;
-        ListNode left = mergeLists(lists, start, mid);
-        ListNode right = mergeLists(lists, mid + 1, end);
-        return merge(left, right);
+
+        int mid = start+(end-start)/2;
+
+        return merge(mergeLists(lists,start,mid), mergeLists(lists,mid+1,end));
     }
 
-    ListNode merge(ListNode l1, ListNode l2) {
+    ListNode merge(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
-        
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
+
+        while(l1 !=null && l2 !=null){
+            if(l1.val <= l2.val){
                 curr.next = l1;
                 l1 = l1.next;
-            } else {
+            }else{
                 curr.next = l2;
                 l2 = l2.next;
             }
             curr = curr.next;
         }
-        
-        // Attach remaining nodes
-        curr.next = (l1 != null) ? l1 : l2;
+        if(l1 == null) curr.next = l2;
+        else curr.next = l1;
         return dummy.next;
     }
 }
