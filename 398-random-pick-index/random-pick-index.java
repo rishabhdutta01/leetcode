124d88a9230bd0e30dd684815d71ad35c3199d7d
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-16 08:59:41 -0700

    Time: 1523 ms (8.20%) | Memory: 55.5 MB (85.81%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -1,27 +1,30 @@
 class Solution {
-    Map<Integer, List<Integer>> map;
+
+    private int[] nums;
+    private Random rand;
+    
     public Solution(int[] nums) {
-        map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i], new ArrayList<>());
-            }
-            map.get(nums[i]).add(i);
-        }
+        this.nums = nums;
+        this.rand = new Random();
     }
     
     public int pick(int target) {
-        List<Integer> cnt = map.get(target);
-        
-        Random r = new Random();
-        int rint = r.nextInt(cnt.size());
-        
-        return cnt.get(rint);
+        int n = this.nums.length;
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            // if nums[i] is equal to target, i is a potential candidate
+            // which needs to be chosen uniformly at random
+            if (this.nums[i] == target) {
+                // increment the count of total candidates
+                // available to be chosen uniformly at random
+                count++;
+                // we pick the current number with probability 1 / count (reservoir sampling)
+                if (rand.nextInt(count) == 0) {
+                    idx = i;
+                }
+            }
+        }
+        return idx;
     }
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(nums);
- * int param_1 = obj.pick(target);
- */
