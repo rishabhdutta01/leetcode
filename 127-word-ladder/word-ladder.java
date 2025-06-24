d5f10091f15771cd9c572335db88a8670c4b11c5
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-23 23:27:02 -0700

    Time: 96 ms (39.42%) | Memory: 46.8 MB (55.66%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,68 +1,55 @@
 class Solution {
-    public int ladderLength(
-        String beginWord,
-        String endWord,
-        List<String> wordList
-    ) {
-        // Since all words are of same length.
-        int L = beginWord.length();
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (beginWord.equals(endWord))
+            return 0;
 
-        // Dictionary to hold combination of words that can be formed,
-        // from any given word. By changing one letter at a time.
-        Map<String, List<String>> allComboDict = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
 
-        wordList.forEach(word -> {
-            for (int i = 0; i < L; i++) {
-                // Key is the generic word
-                // Value is a list of words which have the same intermediate generic word.
-                String newWord =
-                    word.substring(0, i) + '*' + word.substring(i + 1, L);
-                List<String> transformations = allComboDict.getOrDefault(
-                    newWord,
-                    new ArrayList<>()
-                );
-                transformations.add(word);
-                allComboDict.put(newWord, transformations);
-            }
-        });
+        q.add(beginWord);
+        set.addAll(wordList);
+        set.remove(beginWord);
 
-        // Queue for BFS
-        Queue<Pair<String, Integer>> Q = new LinkedList<>();
-        Q.add(new Pair(beginWord, 1));
+        int dist = 1;
 
-        // Visited to make sure we don't repeat processing same word.
-        Map<String, Boolean> visited = new HashMap<>();
-        visited.put(beginWord, true);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String w = q.poll();
 
-        while (!Q.isEmpty()) {
-            Pair<String, Integer> node = Q.remove();
-            String word = node.getKey();
-            int level = node.getValue();
-            for (int i = 0; i < L; i++) {
-                // Intermediate words for current word
-                String newWord =
-                    word.substring(0, i) + '*' + word.substring(i + 1, L);
+                if (endWord.equals(w))
+                    return dist;
 
-                // Next states are all the words which share the same intermediate state.
-                for (String adjacentWord : allComboDict.getOrDefault(
-                    newWord,
-                    new ArrayList<>()
-                )) {
-                    // If at any point if we find what we are looking for
-                    // i.e. the end word - we can return with the answer.
-                    if (adjacentWord.equals(endWord)) {
-                        return level + 1;
-                    }
-                    // Otherwise, add it to the BFS Queue. Also mark it visited
-                    if (!visited.containsKey(adjacentWord)) {
-                        visited.put(adjacentWord, true);
-                        Q.add(new Pair(adjacentWord, level + 1));
+                for(int j=0;j<w.length();j++){
+                    for(char ch = 'a';ch<='z';ch++){
+                        char[] arr = w.toCharArray();
+                        arr[j] = ch;
+                        String rw = new String(arr);
+                        if(set.contains(rw)){
+                            set.remove(rw);
+                            q.add(rw);
+                        }
                     }
                 }
             }
+            dist++;
         }
-
         return 0;
+    }
+
+    boolean adjacent(String w1, String w2) {
+        if (w1.length() != w2.length())
+            return false;
+
+        int cnt = 0;
+        for (int i = 0; i < w1.length(); i++) {
+            if (w1.charAt(i) != w2.charAt(i)) {
+                cnt++;
+                if (cnt == 2)
+                    return false;
+            }
+        }
+        return cnt == 1;
     }
 }
 // class Solution {
