17a1ceebddb209ecd5746042cad267d710f55a04
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-06-24 16:11:54 -0700

    Time: 138 ms (65.72%) | Memory: 41.5 MB (77.73%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -20,19 +20,22 @@ class Solution {
         if(k == word.length()) {
             return true;
         }
+
         if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] != word.charAt(k)){
             return false;
         }
-        boolean res = false;
+
         if(board[i][j] == word.charAt(k)){
             char temp = board[i][j];
             board[i][j] = '#';
-            res = go(board, word, i-1, j, k+1) 
+            if(go(board, word, i-1, j, k+1) 
                 || go(board, word, i+1, j, k+1)
                 || go(board, word, i, j-1, k+1)
-                || go(board, word, i, j+1, k+1);
+                || go(board, word, i, j+1, k+1)){
+                    return true;
+                }
             board[i][j] = temp;
         }
-        return res;
+        return false;
     }
 }
