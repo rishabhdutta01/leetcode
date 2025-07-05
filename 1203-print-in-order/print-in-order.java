5f063fab142c4fbbd06f68394f48a93083d6db8d
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-07-04 19:23:02 -0700

    Time: 12 ms (55.38%) | Memory: 42.9 MB (5.07%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -1,32 +1,19 @@
 class Foo {
+    private boolean firstDone, secondDone;
 
-  private AtomicInteger firstJobDone = new AtomicInteger(0);
-  private AtomicInteger secondJobDone = new AtomicInteger(0);
-
-  public Foo() {}
-
-  public void first(Runnable printFirst) throws InterruptedException {
-    // printFirst.run() outputs "first".
-    printFirst.run();
-    // mark the first job as done, by increasing its count.
-    firstJobDone.incrementAndGet();
-  }
-
-  public void second(Runnable printSecond) throws InterruptedException {
-    while (firstJobDone.get() != 1) {
-      // waiting for the first job to be done.
+    public void first(Runnable printFirst) {
+        printFirst.run();
+        firstDone = true;
     }
-    // printSecond.run() outputs "second".
-    printSecond.run();
-    // mark the second as done, by increasing its count.
-    secondJobDone.incrementAndGet();
-  }
 
-  public void third(Runnable printThird) throws InterruptedException {
-    while (secondJobDone.get() != 1) {
-      // waiting for the second job to be done.
+    public void second(Runnable printSecond) {
+        while (!firstDone) Thread.onSpinWait();
+        printSecond.run();
+        secondDone = true;
     }
-    // printThird.run() outputs "third".
-    printThird.run();
-  }
+
+    public void third(Runnable printThird) {
+        while (!secondDone) Thread.onSpinWait();
+        printThird.run();
+    }
 }
