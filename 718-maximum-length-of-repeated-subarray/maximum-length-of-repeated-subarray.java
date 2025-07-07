554f2d2bf4873f3b10913d87b4598967b27f5745
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-07-07 12:44:33 -0700

    Time: 26 ms (39.93%) | Memory: 55.1 MB (37.71%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -1,23 +1,19 @@
-class Solution {
+class Solution {    
     public int findLength(int[] nums1, int[] nums2) {
-        // 2 passes because we need to have the 2 arrays fully pass eachother 
-        return Math.max(helper(nums1,nums2),helper(nums2,nums1));
-    }
-    
-    private int helper(int[] arr1, int[] arr2){
-        int max = 0;
-        // move arr1 forward one step at a time
-        for(int i = 1; i <= arr1.length; i++){
-            // set the comparison starting points of both arr1 and arr2
-            int arr1Index = arr1.length - i;
-            int arr2Index = 0;
-            int matched = 0;
-            while(arr1Index < arr1.length && arr2Index < arr2.length){
-                // increment match count each time a match is found; reset otherwise
-                matched = arr1[arr1Index++] == arr2[arr2Index++]? matched + 1: 0;
-                max = Math.max(max,matched);
+        int n= nums1.length;
+        int m= nums2.length;
+        int ans=0;
+        int dp[][]= new int[n+1][m+1];
+        
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++){
+                
+                if(nums1[i-1]==nums2[j-1] ){
+                    dp[i][j]= 1+ dp[i-1][j-1];
+                    ans=Math.max (ans,dp[i][j] );
+                } 
+                else dp[i][j]=0;
             }
-        }
-        return max;
+        return ans;
     }
 }
