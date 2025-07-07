e13c2d5187f22e4ca76fffde3172cb7cce8509c8
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-07-07 12:39:21 -0700

    Time: 24 ms (53.84%) | Memory: 54.9 MB (47.01%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -1,28 +1,19 @@
-class Solution {
+class Solution {    
     public int findLength(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int max = 0;
-        for(int i =0;i<n1;i++){
-            
-            for(int j=0;j<n2;j++){
-                int cnt = 0;
-                if(nums1[i] == nums2[j]){
-                    cnt += 1;
-                    int idxi = i+1;
-                    int idxj = j+1;
-                    while(idxi != n1 && idxj != n2) {
-                        if(nums1[idxi] == nums2[idxj]) {
-                            idxi++;
-                            idxj++;
-                            cnt++;
-                        } else{break;}
-                    }
-                    max = Math.max(max, cnt);
-                    if(idxi == n1) return max;
-                }
+        int n= nums1.length;
+        int m= nums2.length;
+        int ans=0;
+        int dp[][]= new int[n+1][m+1];
+        
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++){
+                
+                if(nums1[i-1]==nums2[j-1] ){
+                    dp[i][j]= 1+ dp[i-1][j-1];
+                    ans=Math.max (ans,dp[i][j] );
+                } 
+                else dp[i][j]=0;
             }
-        }
-        return max;
+        return ans;
     }
 }
