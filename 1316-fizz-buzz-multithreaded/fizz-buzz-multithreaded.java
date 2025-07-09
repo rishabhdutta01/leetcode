c55d977ae8b1b5d30ec0e4ac1f5f97e2f1c06bc4
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-07-09 16:40:31 -0700

    Time: 6 ms (87.31%) | Memory: 42.6 MB (72.33%) - LeetSync

---
+++ 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.java
@@ -0,0 +1,65 @@
+class FizzBuzz {
+    private int n;
+    private volatile int current = 1;
+    private final Object lock = new Object();
+    
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && (current % 3 != 0 || current % 5 == 0)) {
+                    lock.wait();
+                }
+                if (current > n) break;
+                printFizz.run();
+                current++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && (current % 5 != 0 || current % 3 == 0)) {
+                    lock.wait();
+                }
+                if (current > n) break;
+                printBuzz.run();
+                current++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && current % 15 != 0) {
+                    lock.wait();
+                }
+                if (current > n) break;
+                printFizzBuzz.run();
+                current++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && (current % 3 == 0 || current % 5 == 0)) {
+                    lock.wait();
+                }
+                if (current > n) break;
+                printNumber.accept(current);
+                current++;
+                lock.notifyAll();
+            }
+        }
+    }
+}
