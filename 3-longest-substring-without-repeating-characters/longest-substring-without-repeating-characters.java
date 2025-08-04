e00d81a4e14e4098248f8883e87f34e9e8e8a77c
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-08-04 16:32:57 -0700

    Time: 13 ms (14.25%) | Memory: 44.5 MB (79.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,58 +1,26 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s==null || s.length() == 0){
-            return 0;
-        }
-        if(s.length() == 1){
-            return 1;
-        }
+        if(s == null  || s.length() == 0) return 0;
 
-        Map<Character, Integer> m = new HashMap<>();
-        int l=0, r=1, res = 1;
-        m.put(s.charAt(l), l);
-        while(r<s.length()){
-            if(!m.containsKey(s.charAt(r))){
-                m.put(s.charAt(r), r);
-                res = Math.max(res, r-l+1);
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        map.put(s.charAt(0), 0);
+        int cnt = 1;
+        int l=0;
+        for(int r=1;r<n;r++){
+            char c = s.charAt(r);
+            if(!map.containsKey(c)){
+                map.put(c, r);
+                cnt = Math.max(cnt, map.size());
             } else{
-                int target = m.get(s.charAt(r));
-                while(l<=target){
-                    m.remove(s.charAt(l));
+                int idx = map.get(c);
+                while(l<=idx){
+                    map.remove(s.charAt(l));
                     l++;
                 }
-                m.put(s.charAt(r), r);
+                map.put(c, r);
             }
-            r++;
         }
-        return res;
+        return cnt;
     }
 }
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if(s== null || s.length() == 0){
-//             return 0;
-//         }
-
-//         int l=0;
-//         int r=1;
-//         Map<Character, Boolean> m = new HashMap<>();
-//         m.put(s.charAt(0), true);
-//         int res = 1;
-
-//         while(r<s.length()){
-//             Character c = s.charAt(r);
-//             if(m.containsKey(c) && m.get(c)){
-//                 res = Math.max(res, r-l);
-//                 while(c != s.charAt(l)){
-//                     m.put(s.charAt(l), false);
-//                     l++;
-//                 }
-//                 l++;
-//             }
-//             m.put(c, true);
-//             r++;
-//         }
-//         return Math.max(res, r-l);
-//     }
-// }
