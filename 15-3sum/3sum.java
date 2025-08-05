56733e40e13d11a9426d9de2d4f70a96264424d0
rishabhdutta01 <rishabh.dutta01@gmail.com>
2025-08-04 18:24:51 -0700

    Time: 300 ms (20.46%) | Memory: 51.6 MB (47.96%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
         Arrays.sort(nums);
-        
-        for(int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
-            if( i == 0 || nums[i] != nums[i-1]) {
-                twoSum(i, nums,result);
-            }
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0;i<nums.length && nums[i]<=0;i++){
+            if(i==0 || nums[i-1] != nums[i]) 
+                fnc(nums, i, res);
         }
-        return result;
+        return res;
     }
 
-    private void twoSum(int i, int nums[], List<List<Integer>> result) {
-         int low = i+1;
-         int high = nums.length - 1;
-         int compl = -nums[i];
-
-         while(low < high) {
-             int sum = nums[low] + nums[high];
-             if(sum > compl) {
-                 high--;
-                 continue;
-             }
-             if(sum < compl) {
-                 low++;
-                 continue;
-             }
-             result.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
-             while(low < high && nums[low] == nums[low-1]){
-                low++;
-             }
-         }   
-         
+    void fnc(int[] nums, int i, List<List<Integer>> res){
+        Set<Integer> m = new HashSet<>();
+        for(int j=i+1;j<nums.length;j++){
+            int complement = -nums[i] -nums[j];
+            if(m.contains(complement)){
+                res.add(Arrays.asList(nums[i], nums[j], complement));
+                while(j+1<nums.length && nums[j]==nums[j+1]){
+                    j++;
+                }
+            } 
+            m.add(nums[j]);           
+        }
     }
 }
